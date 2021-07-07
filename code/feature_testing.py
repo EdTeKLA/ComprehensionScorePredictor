b713e8670028cb4538dbf4f0d398ff6f008f5638
Bruce Nie <ynie@ualberta.ca>
2021-07-07 17:48:31 -0600

    Update test for correlation between variables
    Signed-off-by: Bruce Nie <ynie@ualberta.ca>

--- code/feature_testing.py
+++ code/feature_testing.py
@@ -1,11 +1,16 @@
 import pandas as pd
 from scipy.stats import spearmanr
 from scipy.stats import pearsonr
+import itertools
+import numpy as np
 
 feature_path = "../data/gr3/gr3_features.xlsx"
 
 df = pd.read_excel(feature_path)
 print(df.columns)
+
+# testing for correlation between the vairable and the target to check for
+# significance
 for col in df.columns[1:-1]:
     test_var = df[col]
     score = df["G3.Gates.RC.raw"]
@@ -17,3 +22,10 @@ for col in df.columns[1:-1]:
 
     corr, _ = pearsonr(test_var, score)
     print("Pearson's correlation: %.3f" % corr)
+
+# testing for correlated variables to avoid redundancy
+for a,b in itertools.combinations(df.columns[1:-1],2):
+    corr, _ = pearsonr(df[a], df[b])
+    if corr > 0.5:
+        print(a,b)
+        print(corr)
