d196d3ae8fd8af3161b9eb0c6353d230a53d51dc
Bruce Nie <ynie@ualberta.ca>
2021-07-20 14:05:51 -0600

    Added data analysis notebook for aquiring descriptive statistics
    Signed-off-by: Bruce Nie <ynie@ualberta.ca>

---
+++ code/build_regress_data.py
@@ -0,0 +1,40 @@
+import pandas as pd
+from scipy.stats import spearmanr
+from scipy.stats import pearsonr
+import itertools
+import numpy as np
+
+feature_path = "../SoR_Alberta.Shared.Data.and.Codebook.xlsx"#"../data/gr3/gr3_features.xlsx"
+feature_names = ['G3.PPVT.Vocab.raw',
+                 'G3.Elision.PA.raw',
+                 'G3.Syn.GramCorrect.raw',
+                 'G4.TOWRE.SWE.raw',
+                 'G4.TOWRE.PDE.raw',
+                 'G4.WordID.raw',
+                 'G3.OL.Spell.Total',
+                 'G3.OL.OrthoChoice.1.2.Total',
+                 'G3.DigitSpan.raw',
+                 'G3.Gates.RC.raw',
+                 'G4.Gates.RC.raw',]
+target_path = "../data/regression/gr3-gr4.csv"
+score_name = "G3.Gates.RC.raw"
+
+df = pd.read_excel(feature_path)
+
+data = {}
+for name in feature_names:
+    data[name] = []
+for i in df.index:
+    unavailable = False
+    for col in feature_names:
+        if df[col][i] < 0:
+            unavailable = True
+            break
+    
+    if unavailable:
+        continue
+    for name in feature_names:
+        data[name].append(df[name][i])
+
+target_df = pd.DataFrame(data, columns = feature_names)
+target_df.to_csv(target_path, index=False)
