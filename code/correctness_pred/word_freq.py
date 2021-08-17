ebfa99aa0ae50a99f43973ad7388c2eeab250590
Bruce Nie <ynie@ualberta.ca>
2021-08-17 15:21:52 -0600

    Added script to preprosses and children's book data and generate common word list
    Signed-off-by: Bruce Nie <ynie@ualberta.ca>

---
+++ code/correctness_pred/word_freq.py
@@ -0,0 +1,77 @@
+import string
+import pickle
+from nltk import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+from collections import Counter
+
+from matplotlib import pyplot as plt
+
+def preprocess(corpus):
+    corpus = corpus.lower()
+
+    corpus = corpus.replace('_',' ')
+    corpus = "".join([char for char in corpus if char not in string.punctuation])
+
+    words = word_tokenize(corpus)
+
+    # print(words[:20])
+
+    stop_words = stopwords.words('english')
+    # print(stop_words)
+    filtered_words = [word for word in words if word not in stop_words]
+
+    # print(filtered_words[:20])
+
+    
+    lemmatizer = WordNetLemmatizer()
+    lemmatized = [lemmatizer.lemmatize(word) for word in filtered_words]
+
+    porter = PorterStemmer()
+    stemmed = [porter.stem(word) for word in lemmatized]
+
+    return stemmed
+
+with open("../../data/cbt_test.txt",'r') as file:
+    corpus = file.read().replace('\n', ' ')
+words = preprocess(corpus)
+freq = Counter(words)
+
+print('vocab size:', len(freq))
+# for key in list(freq.keys())[0:10]:
+#     print(key, freq[key])
+
+ranking = sorted(freq.items(),key=lambda item:item[1],reverse=True)
+
+threshold = 5
+rare_words = []
+wordlist = []
+data_pts = []
+
+for i in range(0,len(ranking),5):
+    value = ranking[i][1]
+    data_pts.append(value)
+
+for i in range(len(ranking)):
+    if ranking[i][1] <= threshold:
+        rare_words.append(ranking[i][0])
+    else:
+        wordlist.append(ranking[i][0])
+
+print("First 50 rare words:\n", rare_words[:50])
+print("Last 50 common words\n", wordlist[:50])
+print("Number of words in common words list:\n", len(wordlist))
+print("Number of words in rare words list:\n", len(rare_words))
+
+with open("wordlist.pkl",'wb') as fp:
+    pickle.dump(wordlist, fp)
+
+# plt.plot(data_pts)
+# plt.title('Sorted word frequency')
+# plt.show()
+
+# lemmatizer = WordNetLemmatizer()
+# print(lemmatizer.lemmatize('corpora'))
+# print(lemmatizer.lemmatize('better'))
